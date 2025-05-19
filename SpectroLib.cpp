9ce1c8a25d9edff1186817d4789c206550498402
Luc654 <lucas.dieleman@outlook.com>
2025-05-19 11:26:19 +0200

    added begin / testDisplay function

--- SpectroLib.cpp
+++ SpectroLib.cpp
@@ -1,10 +1,43 @@
 #include "SpectroLib.h"
+#include <Wire.h>
 #include <Adafruit_SH110X.h>
 
 SpectroLib::SpectroLib(Adafruit_SH1106G *display) {
   _display = display;
 }
 
-void SpectroLib::test(){
-    Serial.println("aa");
+
+void SpectroLib::begin(){
+  
+  Wire.begin();  
+  _display->begin(0x3C, true);  
+
+  _display->clearDisplay();
+  _display->setTextSize(1);      
+  _display->setTextColor(SH110X_WHITE);
+}
+
+void SpectroLib::testDisplay(){
+  showText("Hello world", 10,10);
+}
+
+
+
+
+
+
+
+
+
+// ========================================
+// Private functions
+// ========================================
+
+void SpectroLib::showText(const char *text, int xPos, int yPos) {
+  _display->clearDisplay();
+  Serial.print("Setting text: ");
+  Serial.println(text);
+  _display->setCursor(xPos, yPos);
+  _display->println(text);
+  _display->display();
 }
--- SpectroLib.h
+++ SpectroLib.h
@@ -4,8 +4,10 @@
 class SpectroLib{
     public:
         SpectroLib(Adafruit_SH1106G* displayObject); 
-        void test();
+        void begin();
+        void testDisplay();
 
     private:
         Adafruit_SH1106G* _display; 
+        void showText(const char* text, int xPos, int yPos);
 };
